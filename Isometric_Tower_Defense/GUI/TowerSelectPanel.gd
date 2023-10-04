4565092a247c31f5636a593a6037f2b78ad57097
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-10-05 00:48:18 +0100

    Tower selection boxes tween in and out when mouse hovers over them

--- Isometric_Tower_Defense/GUI/TowerSelectPanel.gd
+++ Isometric_Tower_Defense/GUI/TowerSelectPanel.gd
@@ -11,5 +11,28 @@ extends Panel
 		$VBoxContainer/HBoxContainer/Label.text = str(tower_price)
 @export var texture : Texture2D:
 	set(new_texture):
-		texture = texture
-		$TextureRect.texture = new_texture
+		texture = new_texture
+		$TextureRect.texture = texture
+@export var y_travel : int = 50
+@export var separation : int = 20
+
+func _ready() -> void:
+	_on_mouse_exited()
+
+func _on_mouse_entered():
+	var tween = get_tree().create_tween()
+	var x_offset : int = 0
+	for p in get_parent().get_children():
+		if p == self:
+			break
+		x_offset += p.custom_minimum_size.x + separation
+	tween.tween_property(self, "position", Vector2(x_offset, -y_travel), 1).set_trans(Tween.TRANS_CUBIC)
+
+func _on_mouse_exited():
+	var tween = get_tree().create_tween()
+	var x_offset : int = 0
+	for p in get_parent().get_children():
+		if p == self:
+			break
+		x_offset += p.custom_minimum_size.x + separation
+	tween.tween_property(self, "position", Vector2(x_offset, y_travel), 1).set_trans(Tween.TRANS_CUBIC)
