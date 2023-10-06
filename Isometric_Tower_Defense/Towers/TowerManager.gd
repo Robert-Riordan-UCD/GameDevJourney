61699131ff11eeea1f271928acd095d6d68c40f5
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-10-07 00:19:14 +0100

    Can deselect tower when placing now

--- Isometric_Tower_Defense/Towers/TowerManager.gd
+++ Isometric_Tower_Defense/Towers/TowerManager.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var selected_tower : PackedScene = null
 
 func _input(event):
-	if event.is_action_pressed("left_mouse"):
+	if selected_tower != null and event.is_action_pressed("left_mouse"):
 		if ground_tile_map.can_place_tower(get_global_mouse_position()):
 			if selected_tower == null: return
 			var new_tower : Tower = selected_tower.instantiate()
@@ -17,6 +17,9 @@ func _input(event):
 			add_child(new_tower)
 			new_tower.position = ground_tile_map.snap_to_grid(get_global_mouse_position())
 			new_tower.request_level_up.connect(_request_level_up.bind(new_tower))
+			selected_tower = null
+	elif event.is_action_pressed("right_mouse"):
+		selected_tower = null
 
 func _request_level_up(cost: int, tower: Tower) -> void:
 	if !level_manager.spend_gold(cost):
