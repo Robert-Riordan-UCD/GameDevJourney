f7d97c73a975d17a448ca42ddf8a6b6188b3544d
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-10-19 00:15:04 +0100

    Added background music

--- Isometric_Tower_Defense/Audio/AudioManager.gd
+++ Isometric_Tower_Defense/Audio/AudioManager.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var timeout_max : float = 1.0
 
 @onready var timed_out := {}
+@onready var audio_stream_player = $AudioStreamPlayer
 
 func _ready():
 	randomize()
@@ -19,3 +20,6 @@ func request_sound(audio_player: AudioStreamPlayer) -> bool:
 func _timeout(audio_player: AudioStreamPlayer) -> void:
 	if audio_player in timed_out:
 		timed_out.erase(audio_player)
+
+func _on_audio_stream_player_finished():
+	audio_stream_player.play()
