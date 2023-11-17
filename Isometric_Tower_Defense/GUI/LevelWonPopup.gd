c7a7fc1f47f3af3e0a67ad64c6b9ed33f80f492f
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-11-17 22:18:40 +0000

    No next level button if next level not specified

--- Isometric_Tower_Defense/GUI/LevelWonPopup.gd
+++ Isometric_Tower_Defense/GUI/LevelWonPopup.gd
@@ -2,6 +2,10 @@ extends PopupPanel
 
 @export var next_level_path: String
 
+func _ready():
+	if next_level_path == '':
+		$VBoxContainer/NextLevelButton.queue_free()
+
 func display() -> void:
 	if visible: return
 	popup_centered()
