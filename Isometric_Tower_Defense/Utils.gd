37fe717f11fd6ad994fa0200d3dc42201688358f
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-12-24 23:37:06 +0000

    Global utils tween in and out happens from the center for control nodes now

--- Isometric_Tower_Defense/Utils.gd
+++ Isometric_Tower_Defense/Utils.gd
@@ -1,12 +1,18 @@
 extends Node
 
-func tween_in(node: Node2D, duration : float = 1.0) -> void:
+func tween_in(node, duration : float = 1.0) -> void:
 	var tween: Tween = create_tween()
-	var initial_scale := node.scale
+	var initial_scale : Vector2 = node.scale
 	node.scale = Vector2.ZERO
+	if node is Control:
+		node.pivot_offset = node.size/2
+	node.visible = true
 	tween.tween_property(node, "scale", initial_scale, duration).set_trans(Tween.TRANS_ELASTIC)
 
-func tween_out(node: Node2D, duration : float = 1.0) -> void:
+func tween_out(node, duration : float = 1.0) -> void:
 	var tween: Tween = create_tween()
+	if node is Control:
+		node.pivot_offset = node.size/2
 	tween.tween_property(node, "scale", Vector2.ZERO, duration).set_trans(Tween.TRANS_CIRC)
 	await tween.finished
+	node.visible = false
