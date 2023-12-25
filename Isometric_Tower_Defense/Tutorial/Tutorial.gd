a8c51478712d9ea2a1346161c7a5edb9648c5ba0
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-12-25 00:06:14 +0000

    Setup the start of a tutorial level with a popup to tell the player what to do

---
+++ Isometric_Tower_Defense/Tutorial/Tutorial.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var popup_text : Array = [
+	"The orcs are trying to steal your crystals!",
+	"Protect them by building a crossbow tower."
+]
+
+@onready var place_crossbow_tower_popup := $PlaceCrossbowTowerPopup
+@onready var label = $PlaceCrossbowTowerPopup/MarginContainer/VBoxContainer/Label
+
+func _ready():
+	advance()
+
+func advance():
+	label.text = popup_text.pop_front()
+	print(label.text)
+	Utils.tween_in(place_crossbow_tower_popup)
+
+func _on_button_pressed():
+	await Utils.tween_out(place_crossbow_tower_popup)
+	print("Finished waiting")
+	advance()
