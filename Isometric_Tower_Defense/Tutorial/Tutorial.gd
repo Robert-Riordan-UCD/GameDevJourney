d804812bffc12f31673b3ba559309cde6be94de2
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-12-26 02:44:31 +0000

    Tutorial now highlights the next next and advances when it is complete

--- Isometric_Tower_Defense/Tutorial/Tutorial.gd
+++ Isometric_Tower_Defense/Tutorial/Tutorial.gd
@@ -1,23 +1,49 @@
 extends Control
 
-var popup_text : Array = [
-	"The orcs are trying to steal your crystals!",
-	"Protect them by building a crossbow tower."
-]
-
 @onready var place_crossbow_tower_popup := $PlaceCrossbowTowerPopup
 @onready var label = $PlaceCrossbowTowerPopup/MarginContainer/VBoxContainer/Label
+@onready var button = $PlaceCrossbowTowerPopup/MarginContainer/VBoxContainer/Button
 
 func _ready():
-	advance()
+	advance_text("The orcs are trying to steal your crystals!")
+	await button.pressed
+	await Utils.tween_out(place_crossbow_tower_popup)
+	
+	advance_text("Protect them by building a crossbow tower.")
+	await button.pressed
+	await Utils.tween_out(place_crossbow_tower_popup)
+	
+	focus_target($"../TowerSelect/HBoxContainer/CrossbowSelect")
+	await $"../TowerSelect/HBoxContainer/CrossbowSelect".tower_selected
 
-func advance():
-	if popup_text.is_empty(): return
-	label.text = popup_text.pop_front()
-	print(label.text)
+	focus_target($TowerPlacmentSpot, 0.6, 0.05)
+	await $"../../TowerManager".tower_placed
+	
+	focus_target($TowerPlacmentSpot, 1.0, 1.0)
+
+
+func advance_text(text):
+	label.text = text
 	Utils.tween_in(place_crossbow_tower_popup)
 
-func _on_button_pressed():
-	await Utils.tween_out(place_crossbow_tower_popup)
-	print("Finished waiting")
-	advance()
+func focus_target(node, duration=1.0, radius=0.15):
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_method(
+		_update_shader_position,
+		$FocusShader.material.get_shader_parameter("target"),
+		(node.global_position+node.size/2) / get_viewport_rect().size,
+		duration
+	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_method(
+		_update_shader_radius,
+		$FocusShader.material.get_shader_parameter("radius"),
+		radius,
+		duration
+	).set_trans(Tween.TRANS_ELASTIC)
+
+func _update_shader_position(target):
+	$FocusShader.material.set_shader_parameter("target", target)
+
+func _update_shader_radius(radius):
+	$FocusShader.material.set_shader_parameter("radius", radius)
