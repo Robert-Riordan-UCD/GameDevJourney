f28076c2de5d8a95a13f2dc62d23de24b73f9cee
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-12-27 13:52:08 +0000

    Toggled mouse input on and off during the tutorial

--- Isometric_Tower_Defense/Tutorial/Tutorial.gd
+++ Isometric_Tower_Defense/Tutorial/Tutorial.gd
@@ -9,29 +9,36 @@ func _ready():
 	
 	# Tower placement
 	await advance_text("Protect them by building a crossbow tower.")
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 	focus_target($"../TowerSelect/HBoxContainer/CrossbowSelect")
 	await $"../TowerSelect/HBoxContainer/CrossbowSelect".tower_selected
 	focus_target($TowerPlacmentSpot, 0.6, 0.05)
 	await $"../../TowerManager".tower_placed
+	mouse_filter = Control.MOUSE_FILTER_STOP
 	defocus()
 	
 	# Start button
 	await advance_text("It looks like your prepared. Call the first wave now.")
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 	$"../NextWaveButton".visible = true
 	focus_target($"../NextWaveButton", 0.6, 0.1)
 	await $"../NextWaveButton".next_wave_button_pressed
+	mouse_filter = Control.MOUSE_FILTER_STOP
 	defocus()
 	
 	# Upgrade tower
 	await $"../../Enemies".enemy_died
 	$"../../SpawnPoint/WaveTimer".paused = true
 	await advance_text("That orc dropped some gold! You can upgrade your crossbow now!")
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 	focus_target($TowerPlacmentSpot, 0.6, 0.1)
 	await $"../../TowerManager".tower_leveled_up
+	mouse_filter = Control.MOUSE_FILTER_STOP
 	defocus()
 	
 	# Next wave button
 	await advance_text("The next wave will be coming soon, but if you call them early you'll get some extra gold!")
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 	$"../../SpawnPoint/WaveTimer".paused = false
 	
 	# Place mage tower
