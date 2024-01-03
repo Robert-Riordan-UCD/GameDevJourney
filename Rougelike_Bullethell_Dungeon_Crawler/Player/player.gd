31ca27d791151b1ba93a27522833bd31c333cd65
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-03 00:28:08 +0000

    Added a player with some basic movement

---
+++ Rougelike_Bullethell_Dungeon_Crawler/Player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+@export var player_speed:float = 450.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var speed_left = player_speed * Input.get_axis("player_left", "player_right")
+	var speed_up = player_speed * Input.get_axis("player_up", "player_down")
+	velocity = Vector2(speed_left, speed_up)
+
+	move_and_slide()
+
+	if speed_left:
+		animated_sprite_2d.flip_h = speed_left < 0
+	
+	if velocity:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("default")
