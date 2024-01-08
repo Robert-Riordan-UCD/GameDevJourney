d909f190c146424e6753441d6372ad8cbda3de0d
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-08 17:15:13 +0000

    Player can run through enemies now once they are dead

--- Rougelike_Bullethell_Dungeon_Crawler/Enemies/enemy.gd
+++ Rougelike_Bullethell_Dungeon_Crawler/Enemies/enemy.gd
@@ -16,6 +16,7 @@ func _on_health_died() -> void:
 	died.emit()
 	animated_sprite_2d.position.y -= 16
 	animated_sprite_2d.play("death")
+	$CollisionShape2D.queue_free()
 	await animated_sprite_2d.animation_finished
 	get_parent().remove_child(self)
 	queue_free()
