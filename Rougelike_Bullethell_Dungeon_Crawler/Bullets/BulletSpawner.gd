a84301d5865e09bb753408c1c5b667fa76511eab
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-08 17:42:29 +0000

    Can vary size of bullets and add gaps to bullet patterns

--- Rougelike_Bullethell_Dungeon_Crawler/Bullets/BulletSpawner.gd
+++ Rougelike_Bullethell_Dungeon_Crawler/Bullets/BulletSpawner.gd
@@ -6,6 +6,8 @@ extends Node2D
 @export_category("Bullets")
 @export var bullets_per_second:float = 1
 @export var damage:float = 1
+@export var size:float = 1
+@export var bullets_per_gap:int = -1
 
 @export_category("Rotation")
 #@export_range(-180, 180) var rotation_start:float = -90
@@ -13,6 +15,7 @@ extends Node2D
 @export_range(0, 2*PI) var rotation_speed:float = PI
 
 @onready var bullet_spawn_timer:Timer = $BulletSpawnTimer
+@onready var bullets_till_next_gap:int = bullets_per_gap
 
 const bullet_scene:PackedScene = preload("res://Bullets/bullet.tscn")
 
@@ -31,8 +34,14 @@ func disable():
 	bullet_spawn_timer.stop()
 
 func _on_bullet_spawn_timer_timeout():
+	if bullets_till_next_gap == 0:
+		bullets_till_next_gap = bullets_per_gap
+		return
+	bullets_till_next_gap -= 1
+	
 	var new_bullet:Bullet = bullet_scene.instantiate()
 	new_bullet.global_position = global_position + spawn_radius*Vector2.RIGHT.rotated(rotation)
 	new_bullet.direction = Vector2.RIGHT.rotated(rotation)
 	new_bullet.damage = damage
+	new_bullet.scale *= size
 	get_tree().root.add_child(new_bullet)
