022b4189216a69d0ac96c5d6b4777b108a24b7b5
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-12 22:27:23 +0000

    Enemies won't spwan in wall any more

---
+++ Rougelike_Bullethell_Dungeon_Crawler/World/Rooms/SpawnArea.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready() -> void:
+	randomize()
+
+func get_random_point() -> Vector2:
+	var size:Vector2 = $CollisionShape2D.shape.size
+	return $CollisionShape2D.position + Vector2(randi_range(-size.x/2, size.x/2), randi_range(-size.y/2, size.y/2))
