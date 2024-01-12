526879898cec1ba87664a33112c3a81b562fa6ae
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-12 21:59:00 +0000

    Added i-frames on hit

--- Rougelike_Bullethell_Dungeon_Crawler/Utilites/Hurtbox.gd
+++ Rougelike_Bullethell_Dungeon_Crawler/Utilites/Hurtbox.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 @export var parent:Node
 @export var team:String = 'enemy'
+@export var invincibility_time:float = 0.2
+
+var invincibile:bool = false
 
 func _init() -> void:
 	collision_layer = 0
@@ -12,7 +15,10 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(hitbox:HitBox) -> void:
-	if not hitbox: return
+	if invincibile or not hitbox: return
 	if hitbox.team == team: return
 	if parent.has_method("take_damage"):
 		parent.take_damage(hitbox.damage)
+	invincibile = true
+	await get_tree().create_timer(invincibility_time).timeout
+	invincibile = false
