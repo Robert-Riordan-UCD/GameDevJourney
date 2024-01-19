9ce8d638000a5a280877172b15db971d070fcc66
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-19 00:30:29 +0000

    Added controller support to the main menu

--- Rougelike_Bullethell_Dungeon_Crawler/GUI/MainMenu.gd
+++ Rougelike_Bullethell_Dungeon_Crawler/GUI/MainMenu.gd
@@ -1,10 +1,28 @@
 extends Control
 
+@onready var buttons: VBoxContainer = $Buttons
+@onready var active_button:int = -1
+@onready var button_timer:Timer = $ButtonTimer
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("player_ui_interact"):
+		buttons.get_children()[active_button].pressed.emit()
+	
+	if button_timer.is_stopped() and event.is_action_pressed("player_up"):
+		active_button = (active_button-1)%buttons.get_child_count()
+		buttons.get_children()[active_button].call_deferred("grab_focus")
+		button_timer.start()
+	
+	if button_timer.is_stopped() and event.is_action_pressed("player_down"):
+		active_button = (active_button+1)%buttons.get_child_count()
+		buttons.get_children()[active_button].call_deferred("grab_focus")
+		button_timer.start()
+
 func _on_play_button_pressed():
 	SceneTransition.change_scene("res://World/Levels/level.tscn")
 
 func _on_credit_button_pressed() -> void:
-	$VBoxContainer/CreditButton.text = "I haven't added this yet"
+	$Buttons/CreditButton.text = "I haven't added this yet"
 	#SceneTransition.change_scene("res://GUI/LevelSelectScreen.tscn")
 
 func _on_quit_button_pressed() -> void:
