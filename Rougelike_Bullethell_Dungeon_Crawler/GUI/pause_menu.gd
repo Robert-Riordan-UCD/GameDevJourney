f7ef31cd96d7049a6cf26eacd5b98c102a61b843
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-19 00:27:41 +0000

    Added a pause menu with controller support

---
+++ Rougelike_Bullethell_Dungeon_Crawler/GUI/pause_menu.gd
@@ -0,0 +1,49 @@
+extends Control
+
+@onready var _paused:bool = false
+@onready var buttons:VBoxContainer = $Buttons
+@onready var active_button:int = -1
+@onready var button_timer:Timer = $ButtonTimer
+
+func _ready() -> void:
+	unpause()
+
+func pause() -> void:
+	_paused = true
+	visible = true
+	get_tree().paused = true
+
+func unpause() -> void:
+	get_tree().paused = false
+	visible = false
+	_paused = false
+	active_button = -1
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("player_pause"):
+		pause()
+	
+	if not _paused: return
+	
+	if event.is_action_pressed("player_ui_interact"):
+		buttons.get_children()[active_button].pressed.emit()
+	
+	if button_timer.is_stopped() and event.is_action_pressed("player_up"):
+		active_button = (active_button-1)%buttons.get_child_count()
+		buttons.get_children()[active_button].call_deferred("grab_focus")
+		button_timer.start()
+	
+	if button_timer.is_stopped() and event.is_action_pressed("player_down"):
+		active_button = (active_button+1)%buttons.get_child_count()
+		buttons.get_children()[active_button].call_deferred("grab_focus")
+		button_timer.start()
+
+func _on_play_button_pressed() -> void:
+	unpause()
+
+func _on_menu_button_pressed() -> void:
+	get_tree().paused = false
+	SceneTransition.change_scene("res://GUI/MainMenu.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
