eb2b58397ffc3be8d4b2867b4afc817eae6b735e
Robert-Riordan <robert.riordan@ucdconnect.ie>
2024-01-20 01:55:07 +0000

    Added hit flash to boss

--- Rougelike_Bullethell_Dungeon_Crawler/Enemies/Boss/StoneGolem/stone_golem.gd
+++ Rougelike_Bullethell_Dungeon_Crawler/Enemies/Boss/StoneGolem/stone_golem.gd
@@ -2,12 +2,17 @@ extends Boss
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var state_machine: Node2D = $StateMachine
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func activate() -> void:
 	state_machine.change_state("Move")
 
+func _on_health_damaged() -> void:
+	animation_player.play("hit")
+
 func _on_health_died() -> void:
 	if _dying:return
+	_on_health_damaged()
 	_dying = true
 	died.emit()
 	await state_machine.change_state("Death")
